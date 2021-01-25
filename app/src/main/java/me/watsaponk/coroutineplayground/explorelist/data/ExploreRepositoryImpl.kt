4259090b9a54e9909d6d76083a37f34ed875e722
Watsapon 'Ty' Kampong <Watsapon.k@kaidee.com>
2021-01-25 09:16:28 +0700

    ref: replace Thread.sleep with delay

--- app/src/main/java/me/watsaponk/coroutineplayground/explorelist/data/ExploreRepositoryImpl.kt
+++ app/src/main/java/me/watsaponk/coroutineplayground/explorelist/data/ExploreRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package me.watsaponk.coroutineplayground.explorelist.data
 
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
 import me.watsaponk.coroutineplayground.explorelist.domain.ExploreRepository
 import me.watsaponk.coroutineplayground.explorelist.domain.ExploreSubject
@@ -8,6 +9,8 @@ import me.watsaponk.coroutineplayground.explorelist.domain.ExploreSubject
 class ExploreRepositoryImpl : ExploreRepository {
 
     override suspend fun getExploreSubjects(): List<ExploreSubject> {
+        // TODO watch this https://www.youtube.com/watch?v=YrrUCSi72E8
+        delay(2_000L)
         return withContext(Dispatchers.IO) {
             getEntities().map { entity ->
                 entity.toModel()
@@ -16,7 +19,6 @@ class ExploreRepositoryImpl : ExploreRepository {
     }
 
     private fun getEntities(): List<ExploreSubjectEntity> {
-        Thread.sleep(2_000L)
         return listOf(
             ExploreSubjectEntity(title = "Basic suspend function", null),
             ExploreSubjectEntity(title = "Coroutine Unit Test", null)
